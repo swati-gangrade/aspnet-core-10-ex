c2d4a00a0d307df03d9a935ff287fe5f60e642bc
Shrutika Mendhe <shrutika.mendhe@click2cloud.net>
2016-07-13 20:12:08 +0530

    code added

---
+++ src/aspnet-core-10-ex/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_10_ex.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string frameworkVersion = "Not available";
+
+            if (Environment.GetEnvironmentVariable("ASPNET_VERSION") != null)
+                frameworkVersion = "Current application running on ASP.NET " + Environment.GetEnvironmentVariable("ASPNET_VERSION").ToString();
+
+            ViewBag.FrameworkVersion = frameworkVersion;
+
+            string environmentVariables = string.Empty;
+            foreach (System.Collections.DictionaryEntry de in Environment.GetEnvironmentVariables())
+                environmentVariables += string.Format("{0} = {1}\n", de.Key.ToString().Trim(), de.Value.ToString().Trim());
+
+            ViewBag.EnvironmentVariables = environmentVariables;
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
